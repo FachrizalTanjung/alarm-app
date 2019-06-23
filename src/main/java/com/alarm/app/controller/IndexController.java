1b5c5dda517ded33cf20b0aeaa1de6acd0cf8f72
wiwitsp <wiwit.putro@btpn.com>
2019-06-23 15:24:33 +0700

    update command

--- src/main/java/com/alarm/app/controller/IndexController.java
+++ src/main/java/com/alarm/app/controller/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 	
+	/*index controller*/
 	@RequestMapping(value = "/")
 	public String index() {
 		return "index";
