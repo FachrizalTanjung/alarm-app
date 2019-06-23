818cdee6c63420060593c0ce0b73f14d889c9726
fachrizal.ta <fachrizal@rumarumi.net>
2019-06-23 17:07:07 +0700

    update database alarm

--- src/main/resources/database/dbalarm.sql
+++ src/main/resources/database/dbalarm.sql
@@ -16,18 +16,55 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`dbalarm` /*!40100 DEFAULT CHARACTER SET
 
 USE `dbalarm`;
 
+/*Table structure for table `kategori` */
+
+DROP TABLE IF EXISTS `kategori`;
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL auto_increment,
+  `kategori` varchar(50) NOT NULL,
+  `file` blob,
+  PRIMARY KEY  (`id_kategori`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `kategori` */
+
 /*Table structure for table `reminder` */
 
 DROP TABLE IF EXISTS `reminder`;
 
 CREATE TABLE `reminder` (
   `id` int(11) NOT NULL auto_increment,
-  `content` text NOT NULL,
-  `active_date` datetime NOT NULL,
-  PRIMARY KEY  (`id`)
+  `catatan` text NOT NULL,
+  `tanggal_aktif` datetime NOT NULL,
+  `tanggal_sebelum_aktif` datetime default NULL,
+  `membuat_tanggal` datetime NOT NULL,
+  `mengupdate_tanggal` datetime default NULL,
+  `membuat_nama` varchar(30) default NULL,
+  `mengupdate_nama` varchar(30) default NULL,
+  `id_kategori` int(11) NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `reminder_fk1` (`membuat_nama`),
+  KEY `reminder_fk2` (`mengupdate_nama`),
+  KEY `reminder_fk3` (`id_kategori`),
+  CONSTRAINT `reminder_fk1` FOREIGN KEY (`membuat_nama`) REFERENCES `user` (`username`),
+  CONSTRAINT `reminder_fk2` FOREIGN KEY (`mengupdate_nama`) REFERENCES `user` (`username`),
+  CONSTRAINT `reminder_fk3` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `reminder` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(12) NOT NULL,
+  PRIMARY KEY  (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
